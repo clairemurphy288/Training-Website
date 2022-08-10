6911c7a818fc694c08af9e66c466e278f9af77bc
Claire <murphyclaire91@gmail.com>
2022-08-10 11:36:42 -0400

    fixed env file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require('express');
 //cors is some middleware still not sure what it does. 
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 ////not sure what this does
@@ -14,8 +14,7 @@ app.use(express.json());
 
 ////////CONNECTING TO MONGODB & MONGOOSE
 // need to add uri to environment varibales. 
-const uri = "mongodb+srv://admin-claire-murphy:DRo4pOC5XjBUWYXh@cluster0.1knex.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully!")
