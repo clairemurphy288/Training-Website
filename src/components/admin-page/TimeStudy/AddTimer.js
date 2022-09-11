b84e75d3a307ff0634380644977834fdb595ad5d
Claire <murphyclaire91@gmail.com>
2022-09-11 16:59:05 -0400

    fixed timer array

--- src/components/admin-page/TimeStudy/AddTimer.js
+++ src/components/admin-page/TimeStudy/AddTimer.js
@@ -1,24 +1,28 @@
 
 import NavBar from "../utilities/navbar"
 import { useState, useEffect } from "react";
+import { useRef } from "react";
 
 export default function AddTimer(props) {
-    const [process, setProcess] = useState([{
+    let process = useRef([{
         text: <textarea className="form-control"></textarea>, 
         button: <button onClick={onClick} className="btn btn-dark mt-2">+</button>
     }]);
-    const [updatedList, setList] = useState([])
-    useEffect(() => {
-        setList(process.map((item) =>  {return <div>{item.text}{item.button}</div>}))
-    }, [process])
+    const [updatedList, setList] = useState(process.current);
     function onClick(e) {
-        let newArray = [...process];
+        let newArray = [...process.current];
+        delete newArray[newArray.length - 1].button;
         newArray.push({
             text: <textarea className="form-control"></textarea>, 
             button:<button onClick={onClick} className="btn btn-dark mt-2">+</button>
-    })
-    console.log(newArray);
-    setProcess(newArray);
+    });
+    process.current = newArray;
+    setList(process.current);
+    }
+
+    function textOnFocus(e) {
+        console.log(e.target);
+
     }
 
     return(
@@ -26,9 +30,7 @@ export default function AddTimer(props) {
             <NavBar/>
             <h1>This page allows the admin to create a timer!</h1>
             <input className="form-control mb-2"></input>
-            <div>{updatedList}</div>
-            
-
+            <div>{updatedList.map((item, index) =>  {return <div onFocus={textOnFocus} className="mb-2">{item.text}{item.button}</div>})}</div>
         </div>
     )
 }
