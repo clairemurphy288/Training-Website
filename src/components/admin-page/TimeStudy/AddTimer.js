300d716267d2b962089fff914b50e46621751149
Claire <murphyclaire91@gmail.com>
2022-09-24 23:17:41 -0400

    no progess

--- src/components/admin-page/TimeStudy/AddTimer.js
+++ src/components/admin-page/TimeStudy/AddTimer.js
@@ -3,25 +3,26 @@ import NavBar from "../utilities/navbar"
 import { useState, useEffect } from "react";
 import { useRef } from "react";
 import FormTimer from './FormTimer';
-import { components } from 'react-select';
-import { set } from 'mongoose';
-
 export default function AddTimer(props) {
-    const [updatedList, setList] = useState([]);
+    const updatedList = useRef([]);
     const [components, setComponents] = useState([]);
     useEffect(() => {
-        const arr = updatedList.map((item, index) =>  {
-            return <FormTimer setComponents = {setComponents} components = {components} index = {index} key = {index}/>})
+        console.log("hi")
+        const arr = updatedList.current.map((item, index) =>  {
+            return <FormTimer updatedList = {updatedList} item = {item} setComponents = {setComponents} components = {components} index = {index} key = {index}/>})
         setComponents(arr)
-    }, [updatedList]);
+    }, [updatedList.current]);
 
+    useEffect(() => {
+        
+
+    }, [components])
     
     function onClick(e) {
-        const newKey = components.length == 0? 0 : components[components.length - 1].key + 1;
-        const arr = [...components, <FormTimer setComponents = {setComponents} components = {components} index = {newKey} key = {newKey}/>];
-        setComponents(arr);
-
-
+        updatedList.current = [...updatedList.current, ""];
+        const arr = updatedList.current.map((item, index) =>  {
+            return <FormTimer updatedList = {updatedList} item = {item} setComponents = {setComponents} components = {components} index = {index} key = {index}/>})
+        setComponents(arr)
     }
 
 
