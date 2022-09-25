22fc8cd8a37f3243f0394e75e97a2857c08177d6
Claire <murphyclaire91@gmail.com>
2022-09-24 22:15:25 -0400

    small updates

--- src/components/admin-page/TimeStudy/AddTimer.js
+++ src/components/admin-page/TimeStudy/AddTimer.js
@@ -3,16 +3,25 @@ import NavBar from "../utilities/navbar"
 import { useState, useEffect } from "react";
 import { useRef } from "react";
 import FormTimer from './FormTimer';
+import { components } from 'react-select';
+import { set } from 'mongoose';
 
 export default function AddTimer(props) {
-    let process = useRef([]);
-    const [updatedList, setList] = useState(process.current);
+    const [updatedList, setList] = useState([]);
+    const [components, setComponents] = useState([]);
+    useEffect(() => {
+        const arr = updatedList.map((item, index) =>  {
+            return <FormTimer setComponents = {setComponents} components = {components} index = {index} key = {index}/>})
+        setComponents(arr)
+    }, [updatedList]);
+
     
     function onClick(e) {
-        let newArray = [...process.current];
-        newArray.push('');
-    process.current = newArray;
-    setList(process.current);
+        const newKey = components.length == 0? 0 : components[components.length - 1].key + 1;
+        const arr = [...components, <FormTimer setComponents = {setComponents} components = {components} index = {newKey} key = {newKey}/>];
+        setComponents(arr);
+
+
     }
 
 
@@ -22,8 +31,7 @@ export default function AddTimer(props) {
             <h1 className='timer-header'>This page allows the admin to create a timer!</h1>
             <label>Title</label>
             <input className="form-control mb-2"></input>
-            <div>{updatedList.map((item, index) =>  {
-                return <FormTimer/>})}
+            <div>{components}
             </div>
             <button onClick={onClick} className="btn btn-dark mt-2">+</button>
         </div>
